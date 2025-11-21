795021d67a5042c47ce7f2b16d5d354fdf6ca695
clcheng <chi.lung.cheng@cern.ch>
2025-11-20 21:03:47 -0800

    Adding inline before constexpr to avoid duplicate symbol errors during linking

--- include/jazelle/BankTraits.hpp
+++ include/jazelle/BankTraits.hpp
@@ -4,15 +4,15 @@
 
 namespace jazelle
 {
-    template <typename T> constexpr std::string_view bank_name = "UNKNOWN";
+    template <typename T> inline constexpr std::string_view bank_name = "UNKNOWN";
     
-    template<> constexpr std::string_view bank_name<MCHEAD>  = "MCHEAD";
-    template<> constexpr std::string_view bank_name<MCPART>  = "MCPART";
-    template<> constexpr std::string_view bank_name<PHPSUM>  = "PHPSUM";
-    template<> constexpr std::string_view bank_name<PHCHRG>  = "PHCHRG";
-    template<> constexpr std::string_view bank_name<PHKLUS>  = "PHKLUS";
-    template<> constexpr std::string_view bank_name<PHWIC>   = "PHWIC";
-    template<> constexpr std::string_view bank_name<PHCRID>  = "PHCRID";
-    template<> constexpr std::string_view bank_name<PHKTRK>  = "PHKTRK";
-    template<> constexpr std::string_view bank_name<PHKELID> = "PHKELID";
+    template<> inline constexpr std::string_view bank_name<MCHEAD>  = "MCHEAD";
+    template<> inline constexpr std::string_view bank_name<MCPART>  = "MCPART";
+    template<> inline constexpr std::string_view bank_name<PHPSUM>  = "PHPSUM";
+    template<> inline constexpr std::string_view bank_name<PHCHRG>  = "PHCHRG";
+    template<> inline constexpr std::string_view bank_name<PHKLUS>  = "PHKLUS";
+    template<> inline constexpr std::string_view bank_name<PHWIC>   = "PHWIC";
+    template<> inline constexpr std::string_view bank_name<PHCRID>  = "PHCRID";
+    template<> inline constexpr std::string_view bank_name<PHKTRK>  = "PHKTRK";
+    template<> inline constexpr std::string_view bank_name<PHKELID> = "PHKELID";
 }
